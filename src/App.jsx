8dd35dba5ee6a6387d6d4c7b97ff927e75852136
Arpit Mohapatra <arpitmohapatra18@gmail.com>
2026-01-03 17:55:29 -0500

    fix missing useEffect import in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { HabitProvider } from './context/HabitContext';
 import { ThemeProvider, useTheme } from './context/ThemeContext';
 import { SettingsProvider, useSettings } from './context/SettingsContext';
